09ccae962db67e18b11e5564439d3f5c64dd67e0
mauro <mauro@thenetcircle.com>
2013-08-21 19:56:22 +0800

    initial commit, config parser works

---
+++ server.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include "util.hpp"
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+  try {
+    util::argument_parser ap(argc, argv);
+    ap.get_config().print_config();
+    //server::generic_tcp(ap.get_config());
+    return 0;
+  } catch (util::bad_args &e) {
+    util::helper helper;
+    helper.print(e.what(), argv[0]);
+    return -1;
+  }
+}
